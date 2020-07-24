905a1be9334ae633b85dc3d0b9a5c1b43fd99e7e
Marco T <tropiano.m@gmail.com>
2020-07-24 16:54:01 -0400

    Implemented Email Function
    - Email function started
    - Need to refine use cases
    - Changed variable names in get_version()

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import subprocess
+import smtplib
+import ssl
 
-#Get Java version and return string
+
+# Get Java version and return string
 def get_version():
-    versionstr = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT).decode('utf-8')
+    in_str = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT).decode('utf-8')
 
-    outputstr = versionstr[versionstr.find('\"')+1:versionstr.find('\"',versionstr.find('\"')+1)]
+    out_str = in_str[in_str.find('\"') + 1:in_str.find('\"', in_str.find('\"') + 1)]
 
-    return outputstr
+    return out_str
 
 
 # Check if Java is installed
 def check_installed():
-
     try:
         get_version()
 
@@ -21,3 +23,12 @@ def check_installed():
     return True
 
 
+# Send email with status message
+def send_status_email(email_receiving, message):
+    # TODO Implement use cases (Results for every process)
+    session = smtplib.SMTP('smtp.gmail.com', 587)
+
+    session.starttls()
+    session.login("javacheckerpython@gmail.com", "Python37")
+    session.sendmail("javacheckerpython@gmail.com", email_receiving, message)
+    session.quit()
